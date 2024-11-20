4433ca3ba50646169cb9ddd86c256fec22eecaa2
Jackson Willian Carbonera <jwccarbonera@outlook.com>
2024-11-20 10:58:14 -0300

    Falha no teste de integração para testar pipeline no pull request

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -26,7 +26,7 @@ describe('Testes de Integração', () => {
     return request(app.getHttpServer())
       .post('/clientes')
       .send(CLIENTE1)
-      .expect(HttpStatus.CREATED);
+      .expect(HttpStatus.OK);
   });
 
   it('Deve buscar os dados do cliente', async () => {
